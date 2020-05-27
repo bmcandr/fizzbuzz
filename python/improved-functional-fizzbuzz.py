a62945e94f89dc04e927344131889a98d038bf4e
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2020-05-27 19:49:14 -0400

    add comments to fizzbuzz method

--- python/improved-functional-fizzbuzz.py
+++ python/improved-functional-fizzbuzz.py
@@ -5,10 +5,14 @@ from functools import reduce
 
 def fizzbuzz(rng: list, triggers: list) -> list:
 
+    # return text if trigger is true; if none are true, return i
     def evaluate(i):
+        # filter triggers to those that evaluate to true
         parts = list(filter((lambda j: j['trigger'](i)), triggers))
+        # return combined text of true triggers if parts is not empty, otherwise return i
         return reduce(lambda x, y: x + y, map(lambda part: part['text'], parts)) if parts else i
 
+    # map evaluate to every element in rng
     return list(map(evaluate, rng))
 
 
