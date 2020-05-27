bbe86d599093dc44bdd9c23615bde3f5750a7166
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2020-05-27 19:39:36 -0400

    update with working version

--- python/improved-functional-fizzbuzz.py
+++ python/improved-functional-fizzbuzz.py
@@ -1,18 +1,20 @@
 # This version takes a functional programming approach. The loop from previous versions is replaced with a call to map.
 
+from functools import reduce
+
+
 def fizzbuzz(rng: list, triggers: list) -> list:
+
     def evaluate(i):
-        result = ''
-        for (text, predicate) in triggers:
-            if predicate(i):
-                result += text
-        return result or i
-  
-    print(list(map(evaluate, rng)))
+        parts = list(filter((lambda j: j['trigger'](i)), triggers))
+        return reduce(lambda x, y: x + y, map(lambda part: part['text'], parts)) if parts else i
+
+    return list(map(evaluate, rng))
 
 
-
-fizzbuzz(range(1, 100), [
-    ('Fizz', lambda i: i % 3 == 0),
-    ('Buzz', lambda i: i % 5 == 0)
+result = fizzbuzz(range(1, 100), [
+    {'text' : 'Fizz', 'trigger' : lambda i: i % 3 == 0},
+    {'text' : 'Buzz', 'trigger' : lambda i: i % 5 == 0},
 ])
+
+print(result)
