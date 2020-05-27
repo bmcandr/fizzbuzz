ad059782e00daf0963e35105f0f614562b69d7a7
Brendan McAndrew <19274445+bmcandr@users.noreply.github.com>
2020-05-27 17:43:37 -0400

    add comments

--- python/parameterized-fizzbuzz.py
+++ python/parameterized-fizzbuzz.py
@@ -1,15 +1,17 @@
 # This is a parameterized, and still procedural, approach to FizzBuzz.
 
-def fizzbuzz(rng: list, triggers):
-    
+def fizzbuzz(rng: list, triggers: list):
     for i in rng:
         result = ''
         for text, trigger in triggers:
-            if i % trigger == 0:
+            if trigger(i):
                 result += text
         print(result or i)
 
 fizzbuzz(range(1, 100),[
-    ('Fizz', 3),
-    ('Buzz', 5)
+    ('Fizz', lambda i: i % 3 == 0),
+    ('Buzz', lambda i: i % 5 == 0)
 ])
+
+## Pros: less repetitive and more flexible
+## Cons: The loop might not be the most efficient approach...
